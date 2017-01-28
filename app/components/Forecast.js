1e080ff01d6536cc7f7861015e70fdc792648b21
Chris <chris.haack@me.com>
2017-01-28 14:16:35 -0500

    bootstrap icons

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -8,10 +8,25 @@ function puke(object) {
 function Forecast(props) {
   const list = props.cityForecastData['data']['list']
   const icons = list.map((obj) =>
-    <img src={"http://openweathermap.org/img/w/" + obj['weather'][0]['icon'] + ".png"} height="100" width="100"/>
+    <div className="col-xs-6 col-md-3">
+      <a href="#" className="thumbnail">
+        <img src={"http://openweathermap.org/img/w/" + obj['weather'][0]['icon'] + ".png"} height="100" width="100"/>
+      </a>
+    </div>
   );
   return (
-    <div> {icons} </div>
+    <div>
+      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center transparentBg space">
+        <h1>{props.header}</h1>
+      </div>
+      <div>
+        <div className="container">
+          <div className="row">
+            {icons}
+          </div> 
+        </div>
+      </div>
+    </div>
   )
 }
 
