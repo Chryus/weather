0585628adc4f05a3b7052c2658848d63624d5e5b
Chris <chris.haack@me.com>
2017-02-07 09:24:46 -0500

    adjust prompt styles

--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -5,24 +5,26 @@ function Prompt(props) {
   return (
     <div className="jumbotron col-sm-6 col-sm-offset-3 text-center transparentBg space">
       <h1>{props.header}</h1>
-      <div className="col-sm-12">
-        <form onSubmit={props.onSubmitLocation}>
-          <div className="form-group">
-            <input 
-              className="form-control" 
-              placeholder={props.location}
-              type="text"
-              onChange={props.onUpdateLocation} 
-              value={props.location} />
-          </div>
-          <div className="form-group col-sm-4 col-sm-offset-4">
-            <button
-              className="btn btn-block btn-success"
-              type="submit">
-                Get Weather
-            </button>
-          </div>
-        </form>
+      <div className="col-sm-10">
+        <div className="col-sm-6 col-sm-offset-4">
+          <form onSubmit={props.onSubmitLocation}>
+            <div className="form-group">
+              <input 
+                className="form-control" 
+                placeholder={props.location}
+                type="text"
+                onChange={props.onUpdateLocation} 
+                value={props.location} />
+            </div>
+            <div className="form-group col-sm-8 col-sm-offset-2">
+              <button
+                className="btn btn-block btn-success"
+                type="submit">
+                  Get Weather
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   )
