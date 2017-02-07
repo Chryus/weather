40dfecaf10c320091bf89b17c83f40624f5f5078
Chris <chris.haack@me.com>
2017-02-07 15:00:45 -0500

    only apply background to prompt component

--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -3,27 +3,29 @@ const PropTypes = React.PropTypes
 
 function Prompt(props) {
   return (
-    <div className="jumbotron col-sm-6 col-sm-offset-3 text-center transparentBg space">
-      <h1>{props.header}</h1>
-      <div className="col-sm-10">
-        <div className="col-sm-6 col-sm-offset-4">
-          <form onSubmit={props.onSubmitLocation}>
-            <div className="form-group">
-              <input 
-                className="form-control" 
-                placeholder={props.location}
-                type="text"
-                onChange={props.onUpdateLocation} 
-                value={props.location} />
-            </div>
-            <div className="form-group col-sm-8 col-sm-offset-2">
-              <button
-                className="btn btn-block btn-success"
-                type="submit">
-                  Get Weather
-              </button>
-            </div>
-          </form>
+    <div className="background-pattern">
+      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center transparentBg space">
+        <h1>{props.header}</h1>
+        <div className="col-sm-10">
+          <div className="col-sm-6 col-sm-offset-4">
+            <form onSubmit={props.onSubmitLocation}>
+              <div className="form-group">
+                <input 
+                  className="form-control" 
+                  placeholder={props.location}
+                  type="text"
+                  onChange={props.onUpdateLocation} 
+                  value={props.location} />
+              </div>
+              <div className="form-group col-sm-8 col-sm-offset-2">
+                <button
+                  className="btn btn-block btn-success"
+                  type="submit">
+                    Get Weather
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
